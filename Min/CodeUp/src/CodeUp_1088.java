6324392b2042cb4bd40c5b6bc76682cc42b584fb
Minnaldo <minnaldo6602@gmail.com>
2019-10-02 02:45:15 +0900

    1002_2

--- Min/CodeUp/src/CodeUp_1088.java
+++ Min/CodeUp/src/CodeUp_1088.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class CodeUp_1087 {
+public class CodeUp_1088 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -12,15 +12,13 @@ public class CodeUp_1087 {
 				
 		int num1 = sc.nextInt();
 		int cnt = 0;
-		int result = 0;
 		
-		while(true) {
-			cnt++;
-			result += cnt;
-			if(result >= num1) {
-				System.out.println(result);
-				return;
-			}
+		for(int i = 1; i <= num1; i++) {
+			
+			if(i % 3 == 0)
+				continue;
+			
+			System.out.print(i + " ");
 		}
 	}
 
--- Min/CodeUp/src/CodeUp_1089.java
+++ Min/CodeUp/src/CodeUp_1089.java
@@ -4,24 +4,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class CodeUp_1087 {
+public class CodeUp_1089 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-				
-		int num1 = sc.nextInt();
-		int cnt = 0;
-		int result = 0;
-		
-		while(true) {
-			cnt++;
-			result += cnt;
-			if(result >= num1) {
-				System.out.println(result);
-				return;
-			}
-		}
-	}
 
+		int a = sc.nextInt();
+		int d = sc.nextInt();
+		int n = sc.nextInt();
+
+		int result = a;
+		for(int i = 1; i < n; i++) {
+			result += d;
+		}
+		
+		System.out.println(result);
+
+	}
 }
